8610132d910ba80edff4bcc5367c4eabde151f88
Mehmet Aktas <mfatihaktas@gmail.com>
2021-10-25 21:32:48 +0300

    Used pytest fixture

--- test_class.py
+++ test_class.py
@@ -1,23 +1,25 @@
 import pytest
 from bowling_game import BowlingGame
 
+@pytest.fixture
+def game():
+	return BowlingGame()
+
 class TestClass:
+
 	def roll_same_value_n_times(self, game, value, n):
 		for _ in range(n):
 			game.roll(value)
 
-	def test_game_w_all_zeros(self):
-		game = BowlingGame()
+	def test_game_w_all_zeros(self, game):
 		self.roll_same_value_n_times(game, value=0, n=20)
 		assert game.score() == 0
 
-	def test_game_w_all_ones(self):
-		game = BowlingGame()
+	def test_game_w_all_ones(self, game):
 		self.roll_same_value_n_times(game, value=1, n=20)
 		assert game.score() == 20
 
-	def test_w_one_spare(self):
-		game = BowlingGame()
+	def test_w_one_spare(self, game):
 		game.roll(3)
 		game.roll(7)
 		game.roll(3)
@@ -25,8 +27,7 @@ class TestClass:
 		self.roll_same_value_n_times(game, value=0, n=16)
 		assert game.score() == 17
 
-	def test_w_two_spares(self):
-		game = BowlingGame()
+	def test_w_two_spares(self, game):
 		game.roll(3)
 		game.roll(7)
 		game.roll(5)
@@ -36,8 +37,7 @@ class TestClass:
 		self.roll_same_value_n_times(game, value=0, n=16)
 		assert game.score() == 30
 
-	def test_w_two_strikes(self):
-		game = BowlingGame()
+	def test_w_two_strikes(self, game):
 		game.roll(10)
 		game.roll(10)
 		game.roll(1)
@@ -45,8 +45,7 @@ class TestClass:
 		self.roll_same_value_n_times(game, value=0, n=14)
 		assert game.score() == 39
 
-	def test_w_spare_in_the_end(self):
-		game = BowlingGame()
+	def test_w_spare_in_the_end(self, game):
 		self.roll_same_value_n_times(game, value=0, n=2*9)
 		game.roll(4)
 		game.roll(4)
@@ -56,8 +55,7 @@ class TestClass:
 		game.roll(2)
 		assert game.score() == 26
 
-	def test_w_strike_in_the_end(self):
-		game = BowlingGame()
+	def test_w_strike_in_the_end(self, game):
 		self.roll_same_value_n_times(game, value=0, n=2*9)
 		game.roll(10)
 		game.roll(10)
